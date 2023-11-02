76d0766938d586e02eec84084c8112080dd24684
Taher Najim <taher.najim@yahoo.com>
2023-11-02 17:14:02 +0800

    embedded clerk userbutton to enable logging in

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
-
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
   return (
-    <main>
-      <h1>CipherSphere</h1>
-    </main>
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
