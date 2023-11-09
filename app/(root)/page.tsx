93b29d4325eb4e3ed4988a9fdeb92815236ec20d
Taher Najim <taher.najim@yahoo.com>
2023-11-09 17:09:50 +0800

    added sign-in and sign-out routes with authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
- 
+
 export default function Home() {
   return (
     <>
-    <h1 className="head-text text-left">Home</h1>
+      <h1 className="head-text text-left">Home</h1>
     </>
   )
 }
