33062d782509e12f5b49296d8d62503d417ed148
Taher Najim <taher.najim@yahoo.com>
2023-11-10 17:22:07 +0800

    changed organizationalswitcher theme to dark

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes"
 
 const Topbar = () => {
   return (
@@ -23,6 +24,7 @@ const Topbar = () => {
         </div>
 
         <OrganizationSwitcher appearance={{
+          baseTheme: dark,
           elements: {
             organizationSwitcherTrigger: "py-2 px-4"
           }
