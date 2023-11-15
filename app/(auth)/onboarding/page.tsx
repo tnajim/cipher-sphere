9033bc76d8756c9c1d17a3265404e94395580a17
Taher Najim <taher.najim@yahoo.com>
2023-11-15 17:37:48 +0800

    created onboarding form backend and validation

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -21,7 +21,7 @@ async function Page() {
             <p className="mt-3 text-base-regular text-light-2">Complete your profile now to use CipherSphere</p>
 
             <section className="mt-9 bg-dark-2 p-10">
-                <AccountProfile />
+                <AccountProfile user={userData} btnTitle="Continue" />
             </section>
         </main>
     )
