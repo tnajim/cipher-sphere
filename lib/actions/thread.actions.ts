c721a6e1e48ce3a3adfc35b3487a8e54a3f874d8
Taher Najim <taher.najim@yahoo.com>
2023-11-30 17:09:42 +0800

    finished fetchPosts, added pagination variables

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -46,4 +46,20 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20) {
         .skip(skipAmount)
         .limit(pageSize)
         .populate({ path: 'author', model: User })
+        .populate({
+            path: 'children',
+            populate: {
+                path: 'author',
+                model: User,
+                select: "_id name parentId image"
+            }
+        })
+
+    const totalPostsCount = await Thread.countDocuments({ parentId: { $in: [null, undefined] } });
+
+    const posts = await postsQuery.exec();
+
+    const isNext = totalPostsCount > skipAmount + posts.length;
+
+    return { posts, isNext }
 }
