ec0499e9ffd5374c451f590bb1248e6dfb31098b
Taher Najim <taher.najim@yahoo.com>
2023-12-14 16:56:39 +0800

    created working thread comment onSubmit (can check on mongoDB)

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -20,6 +20,7 @@ import { usePathname, useRouter } from "next/navigation";
 // import { updateUser } from "@/lib/actions/user.actions";
 import { CommentValdation } from "@/lib/validations/thread";
 import Image from "next/image";
+import { addCommentToThread } from "@/lib/actions/thread.actions";
 // import { createThread } from "@/lib/actions/thread.actions";
 
 interface Props {
@@ -40,14 +41,9 @@ const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
   });
 
   const onSubmit = async (values: z.infer<typeof CommentValdation>) => {
-    // await createThread({
-    //   text: values.thread,
-    //   author: userId,
-    //   communityId: null,
-    //   path: pathname
-    // });
+    await addCommentToThread(threadId, values.thread, JSON.parse(currentUserId), pathname);
 
-    router.push("/");
+    form.reset();
   }
 
   return (
