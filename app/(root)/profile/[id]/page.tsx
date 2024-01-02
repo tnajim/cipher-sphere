81eefc2c8f7bc06dd32adeea057af98f923f1cb8
Taher Najim <taher.najim@yahoo.com>
2024-01-02 17:19:20 +0800

    added thread count to profile

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -38,6 +38,12 @@ async function Page({ params }: { params: { id: string } }) {
                   className="object-contain"
                 />
                 <p className="max-sm:hidden">{tab.label}</p>
+
+                {tab.label === 'Threads' && (
+                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                    {userInfo?.threads?.length}
+                  </p>
+                )}
               </TabsTrigger>
             ))}
           </TabsList>
