3532430ddbcab37fbd89f62525d74c888b5f39bf
Taher Najim <taher.najim@yahoo.com>
2024-01-15 18:32:58 +0800

    added basic imports and auth on the activity page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,5 +1,16 @@
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const Page = async () => {
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect('/onboarding');
+
+  // todo: getActivities
+  
   return (
     <section>
       <h1 className="head-text mb-10">Activity</h1>
