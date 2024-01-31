92e14cada6ac8a7998fda421074c3f2db9925966
Taher Najim <taher.najim@yahoo.com>
2024-01-31 16:53:42 +0800

    display who replied to your thread in activity tab

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,19 +4,21 @@ import { Inter } from "next/font/google";
 import "../globals.css";
 
 export const metadata = {
-    title: "CipherSphere",
-    description: "A Next.js 13 Threads Application"
+  title: "CipherSphere",
+  description: "A Next.js 13 Threads Application"
 }
 const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <ClerkProvider>
-            <html lang="en">
-                <body className={`${inter.className} bg-dark-1`}>
-                    {children}
-                </body>
-            </html>
-        </ClerkProvider>
-    )
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="w-full flex justify-center items-center min-h-screen">
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
 }
